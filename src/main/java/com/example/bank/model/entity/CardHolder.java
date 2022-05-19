27af5e6bb64195b0909c3e954e6bc184ca7fd4a5
TigranTigranyan <T.tigranyan1993@mail.ru>
2022-05-19 15:07:30 +0400

    add entity ch

--- src/main/java/com/example/bank/model/entity/CardHolder.java
+++ src/main/java/com/example/bank/model/entity/CardHolder.java
@@ -1,4 +1,24 @@
 package com.example.bank.model.entity;
 
+import lombok.*;
+import javax.persistence.*;
+@Entity
+@Table(name = "card_holder")
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@AllArgsConstructor
 public class CardHolder {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(name = "address_id")
+    private String addressId;
+    @Column(name = "name")
+    private  String name;
+    @Column(name = "phone")
+    private String phone;
+
+
 }
