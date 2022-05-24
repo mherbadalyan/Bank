64a29638b6d6972a9a04b5430bc90965bf760fdb
Mher <mherbadalyan@gmail.com>
2022-05-24 23:40:00 +0400

    demo version 1.0 corrected account balance

--- src/main/java/com/example/bank/model/entity/Account.java
+++ src/main/java/com/example/bank/model/entity/Account.java
@@ -20,7 +20,7 @@ public class Account {
     @Column(name = "IBAN", unique = true)
     private String IBAN;
 
-    @Column(name = "balance", columnDefinition = "long default 0l")
+    @Column(name = "balance")
     private Long balance = 0L;
     @ManyToOne
     @JoinColumn(name = "bank_id")
